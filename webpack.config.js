283b7e1535c931f4cd360f4e3cd3130fa57223da
Ryo Kawahata <r-kawahata@cresco.co.jp>
2020-05-30 15:18:03 +0900

    create react project

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const publidDir = __dirname + '/public';
+
+module.exports = {
+  entry: [ 
+     './src/index.tsx' 
+   ], 
+   output: { 
+     path: publidDir, 
+     publicPath: '/', 
+     filename: 'main.js' 
+   }, 
+   module: { 
+     rules: [{
+       test: /\.tsx?$/,
+       use: 'ts-loader',
+     }] 
+   }, 
+   resolve: { 
+     extensions: ['.js', '.jsx', '.ts', '.tsx'] 
+   } 
+ }; 
